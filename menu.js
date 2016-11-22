b1567324745cf711525309e379e7f9913f1440c2
1000ch <shogo.sensui@gmail.com>
2016-11-22 18:16:11 +0900

    Initial commit

---
+++ menu.js
@@ -0,0 +1,144 @@
+'use strict';
+const os = require('os');
+const path = require('path');
+const electron = require('electron');
+
+const app = electron.app;
+const shell = electron.shell;
+const appName = app.getName();
+
+const helpSubmenu = [{
+  label: `${appName} Website`,
+  click() {
+    shell.openExternal('https://github.com/1000ch/quail');
+  }
+}, {
+  label: 'Report an Issue...',
+  click() {
+    const body = `
+<!-- Please succinctly describe your issue and steps to reproduce it. -->
+-
+${app.getName()} ${app.getVersion()}
+Electron ${process.versions.electron}
+${process.platform} ${process.arch} ${os.release()}`;
+
+    shell.openExternal(`https://github.com/1000ch/quail/issues/new?body=${encodeURIComponent(body)}`);
+  }
+}];
+
+if (process.platform !== 'darwin') {
+  helpSubmenu.push({
+    type: 'separator'
+  }, {
+    role: 'about',
+    click() {
+      electron.dialog.showMessageBox({
+        title: `About ${appName}`,
+        message: `${appName} ${app.getVersion()}`,
+        detail: 'Created by Shogo Sensui',
+        icon: path.join(__dirname, 'static/Icon.png'),
+        buttons: []
+      });
+    }
+  });
+}
+
+const darwinTpl = [{
+  label: appName,
+  submenu: [{
+    role: 'about'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'services',
+    submenu: []
+  }, {
+    type: 'separator'
+  }, {
+    role: 'hide'
+  }, {
+    role: 'hideothers'
+  }, {
+    role: 'unhide'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'quit'
+  }]
+}, {
+  label: 'Edit',
+  submenu: [{
+    role: 'undo'
+  }, {
+    role: 'redo'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'cut'
+  }, {
+    role: 'copy'
+  }, {
+    role: 'paste'
+  }, {
+    role: 'pasteandmatchstyle'
+  }, {
+    role: 'delete'
+  }, {
+    role: 'selectall'
+  }]
+}, {
+  role: 'window',
+  submenu: [{
+    role: 'minimize'
+  }, {
+    role: 'close'
+  }, {
+    type: 'separator'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'front'
+  }, {
+    role: 'togglefullscreen'
+  }]
+}, {
+  role: 'help',
+  submenu: helpSubmenu
+}];
+
+const otherTpl = [{
+  label: 'File',
+  submenu: [{
+    role: 'quit'
+  }]
+}, {
+  label: 'Edit',
+  submenu: [{
+    role: 'undo'
+  }, {
+    role: 'redo'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'cut'
+  }, {
+    role: 'copy'
+  }, {
+    role: 'paste'
+  }, {
+    role: 'pasteandmatchstyle'
+  }, {
+    role: 'delete'
+  }, {
+    type: 'separator'
+  }, {
+    role: 'selectall'
+  }]
+}, {
+  role: 'help',
+  submenu: helpSubmenu
+}];
+
+const tpl = process.platform === 'darwin' ? darwinTpl : otherTpl;
+
+module.exports = electron.Menu.buildFromTemplate(tpl);
